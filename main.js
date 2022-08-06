ab648e0e57890de7d453e92667f5c3ee49179557
unknown <ajibolamubarak304@gmail.com>
2022-08-06 23:00:04 +0100

    JS

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 const primaryHeader = document.querySelector(".primary-header");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
+
+navToggle.addEventListener("click", () => {
+  primaryNav.hasAttribute("data-visible")
