9d79a4fe16f5e16efbb99d4379c15d9cbb40e993
unknown <ajibolamubarak304@gmail.com>
2022-08-06 23:00:42 +0100

    JS

--- main.js
+++ main.js
@@ -5,3 +5,5 @@ const primaryNav = document.querySelector(".primary-navigation");
 navToggle.addEventListener("click", () => {
   primaryNav.hasAttribute("data-visible")  ? navToggle.setAttribute("aria-expanded", false)
   : navToggle.setAttribute("aria-expanded", true);
+  primaryNav.toggleAttribute("data-visible");
+  primaryHeader.toggleAttribute("data-overlay");
