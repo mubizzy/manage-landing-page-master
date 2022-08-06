a13b3e3819992470aa8e7ac46024b1ad1e0e0dc6
unknown <ajibolamubarak304@gmail.com>
2022-08-06 22:58:46 +0100

    update

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
 const primaryHeader = document.querySelector(".primary-header");
 const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
