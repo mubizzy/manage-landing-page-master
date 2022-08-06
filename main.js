18c1c6bdb090897bf964af0e633dd6001c100a13
unknown <ajibolamubarak304@gmail.com>
2022-08-06 23:00:28 +0100

    JS

--- main.js
+++ main.js
@@ -3,4 +3,5 @@ const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
 
 navToggle.addEventListener("click", () => {
-  primaryNav.hasAttribute("data-visible")
+  primaryNav.hasAttribute("data-visible")  ? navToggle.setAttribute("aria-expanded", false)
+  : navToggle.setAttribute("aria-expanded", true);
